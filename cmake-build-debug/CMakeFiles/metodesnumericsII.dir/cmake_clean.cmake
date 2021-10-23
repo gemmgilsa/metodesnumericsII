72de34ab51ae8a9f645b5e539ae470a5e19b9694
gemmgilsa <gemmagilatz@gmail.com>
2021-10-23 14:01:29 +0200

    printar

---
+++ cmake-build-debug/CMakeFiles/metodesnumericsII.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metodesnumericsII.dir/main.c.obj"
+  "libmetodesnumericsII.dll.a"
+  "metodesnumericsII.exe"
+  "metodesnumericsII.exe.manifest"
+  "metodesnumericsII.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/metodesnumericsII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
