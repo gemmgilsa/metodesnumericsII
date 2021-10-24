0f4d0027385af98839106f753eaa8ea00eb4a4c6
gemmgilsa <gemmagilatz@gmail.com>
2021-10-24 20:34:12 +0200

    ja va

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #define TOL 10e-10
-#define TOP 500
+#define TOP 100000
 
 double f (double,double);
 double g( double, double);
@@ -67,28 +67,38 @@ int main(void) {
                 b[i][j] += g((i + 1) * h, (n + 1) * h);
         }
     }
-    printf("terme independent i aproximació inicial\n");
+    /*printf("terme independent i aproximacio inicial\n");
     for(j=0;j<n;j++){
         for(i=0;i<n;i++){
-            printf(" j = (%d+1) i= (%d+1) bij=%le   uij=%le\n", j, i, b[i][j], x[i][j]);
+            printf(" j = %d i= %d bij=%le   uij=%le\n", j+1, i+1, b[i][j], x[i][j]);
         }
-    }
-    printf("Comença el procés iteratiu\n");
+    }*/
+    printf("Comencem el proces iteratiu\n");
     k=jacobi(b, x, n, TOL, TOP);
     if(k<0){
-        printf("El mètode de jacobi no convergeix \n");
+        printf("El metode de jacobi no convergeix \n");
     }else{
         printf("Jacobi convergeix a la iter %d\n", k);
     }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            x[i][j]=0;
+        }
+    }
     k=gaussseidel(b, x, n, TOL, TOP);
     if(k<0){
-        printf("El mètode de gausseidel no convergeix \n");
+        printf("El metode de gausseidel no convergeix \n");
     }else{
         printf("gausseidel convergeix a la iter %d\n", k);
     }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            x[i][j]=0;
+        }
+    }
     k=SOR(b, x, n, TOL, TOP, w);
     if(k<0){
-        printf("El mètode de SOR no convergeix \n");
+        printf("El metode de SOR no convergeix \n");
     }else{
         printf("SOR convergeix a la iter %d\n", k);
     }
@@ -100,14 +110,19 @@ int main(void) {
     free(x);
     return 0;
 }
-//hola ana guapa
-double f (double x, double y){
+double f(double x, double y){
+    return (x*x-2)*sin(y)+(4-2*y*y)*cos(x);
+}
+double g(double x, double y){
+    return x*x*sin(y)-2*y*y*cos(x);
+}
+/*double f (double x, double y){
     return -2*pow(x,3) + 6*x*pow(y,2);
 }
-//chechiiiiiii
+
 double g (double x, double y){
     return pow(x, 3)*pow(y, 2)-x*pow(y,4);
-}
+}*/
 int jacobi (double **b, double ** x0, int n, double tol, int top){
     int i, j,k, iter=0;
     double **x1, err=10;
@@ -141,14 +156,14 @@ int jacobi (double **b, double ** x0, int n, double tol, int top){
         }
         iter ++;
         err=fabs(norma(x1,x0, n));
-        printar(iter, n, x1, err);
-
-    }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            x0[i][j]=x1[i][j];
+        // printar(iter, n, x1, err);
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                x0[i][j]=x1[i][j];
+            }
         }
     }
+
     for(i=0;i<n;i++){
         free(x1[i]);
     }
@@ -187,6 +202,7 @@ int gaussseidel (double **b, double ** x, int n, double tol, int top){
         }
         iter++;
         err=fabs(norma(x,xaux, n));
+        //printf("%le\n",err);
     }
     for(i=0;i<n;i++){
         free(xaux[i]);
@@ -220,11 +236,12 @@ int SOR (double **b, double ** x, int n, double tol, int top, double w){
                     x[j][i]+=x[j][i+1];
                 if((j!=(n-1)))
                     x[j][i]+=x[j+1][i];
-                x[j][i]*=w/4+ (1-w)*aux;
+                x[j][i]=(x[j][i]*(double)w/4 + (1-w)*aux);
             }
         }
         iter++;
         err=fabs(norma(x,xaux,n));
+        //printf("%le\n",err);
     }
     for(i=0;i<n;i++){
         free(xaux[i]);
